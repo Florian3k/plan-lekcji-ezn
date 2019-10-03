3e5f6e53f1a0f672f2ac826a56b42c99cd3a615c
avemike <fajny.michal.gajek@gmail.com>
2019-10-03 21:02:45 +0200

    day names prototype

---
+++ src/components/DayNames.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/DayNames.css';
+
+export const DayNames: React.FC = () => {
+  return (
+    <div className="day-names">
+      <div className="day">Poniedziałek</div>
+      <div className="day">Wtorek</div>
+      <div className="day">Środa</div>
+      <div className="day">Czwartek</div>
+      <div className="day">Piątek</div>
+    </div>
+  )
+}
--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/Schedule.css';
 import { Hours } from './Hours';
+import { DayNames } from './DayNames';
 
 export const Schedule: React.FC = () => {
   return (
     <main>
       <Hours />
+      <DayNames />
     </main>
   )
 }
