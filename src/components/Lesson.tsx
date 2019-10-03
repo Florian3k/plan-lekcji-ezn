1511d3bc39640b72ac51b92811fedf6947f2c615
avemike <fajny.michal.gajek@gmail.com>
2019-10-04 01:02:01 +0200

    przepraszam fornal

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/Lesson.css';
+interface LessonProps {
+  lesson: any //przepraszam
+}
 
-export const Lesson: React.FC = () => {
+export const Lesson: React.FC<LessonProps> = (props) => {
+  const period = props.lesson.period;
+  const day = props.lesson.day.split("").reverse().indexOf("1");
   return (
-    <div>
-      
+    <div className={`lesson lesson-${period}-${day}`}>
+      <h3>{props.lesson.subject}</h3>
+      <div className="teacher">{props.lesson.teacher}</div>
+      <div className="room">Sala {props.lesson.classroom}</div>
     </div> 
   )
 }
+
--- src/components/ScheduleWireframe.tsx
+++ src/components/ScheduleWireframe.tsx
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/ScheduleWireframe.css';
+import { Lesson } from './Lesson';
 
 interface ClassProps {
-  class: object
+  class: any //przepraszam
 }
 
 export const ScheduleWireframe: React.FC<ClassProps> = props => {
-  console.log(props.class)
+  let lessons:any;
+  if(props.class !== null && props.class !== undefined) {
+    const days = Object.keys(props.class)
+    lessons = days.map(day => {
+      console.log(day)
+      const lessonNumbers = Object.keys(day)
+      return lessonNumbers.map( lesson => (
+        <Lesson lesson={{...props.class[day][lesson], day}} />
+      ))
+    })
+  }
   return (
     <div className="schedule-wireframe">
-      
+      {lessons}
     </div>
   )
 }
