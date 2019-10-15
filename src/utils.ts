122e61808416da1d2684e5a06d152931a7c83b41
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-10-15 13:44:54 +0200

    fix getClassroomTimetable

--- src/utils.ts
+++ src/utils.ts
@@ -73,29 +73,22 @@ export function getTeacherTimetable(timetable: Timetable, selectedTeacher: strin
 export function getClassroomTimetable(timetable: Timetable, selectedClassroom: string) {
   const classroom = timetable.classrooms.find(({ name }) => name === selectedClassroom)
 
-  const lessons = classroom && timetable.lessons
-    .filter(l => l.classroomids.split(',').includes(classroom.id))
-    .map(l => ({
-      ...l,
-      clazz: timetable.classes.find(byId(l.classids)),
-      subject: maybeGetProp(timetable.subjects.find(byId(l.subjectid)), 'name'),
-      week: timetable.weeksdefs.find(byId(l.weeksdefid)),
-      day: timetable.daysdefs.find(byId(l.daysdefid)),
-      teacher: maybeGetProp(timetable.teachers.find(byId(l.teacherids)), 'name'),
-      group: maybeGetProp(timetable.groups.find(byId(l.groupids)), 'name'),
-      // period: timetable.periods.find(byId())
-    }))
-
-  const lessonsIds = classroom && new Set(lessons!.map(x => x.id))
-
-  return lessons && timetable.cards
-    .filter(c => lessonsIds!.has(c.lessonid))
-    .map(c => ({
-      ...c,
-      classroom: maybeGetProp(timetable.classrooms.find(byId(c.classroomids)), 'name'),
-      lesson: lessons.find(l => l.id === c.lessonid),
-    })).map(c => ({
-      ...R.pick(['period', 'weeks', 'days', 'classroom'], c),
-      ...R.pick(['teacher', 'subject', 'group'], c.lesson),
-    })).sort((a, b) => (a.period as any) - (b.period as any))
+  return classroom && timetable.cards
+    .filter(c => c.classroomids === classroom.id)
+    .map(c => {
+      const lesson = timetable.lessons.find(l => l.id === c.lessonid)!
+      return ({
+        ...c,
+        subject: maybeGetProp(timetable.subjects.find(byId(lesson.subjectid)), 'name'),
+        teacher: maybeGetProp(timetable.teachers.find(byId(lesson.teacherids)), 'name'),
+        group: maybeGetProp(timetable.groups.find(byId(lesson.groupids)), 'name'),
+      })
+    }).map(c => {
+        console.log(c.subject)
+        return ({
+        ...R.pick(['period', 'weeks', 'days', 'teacher', 'subject', 'group'], c),
+        clazz: timetable.classes.filter(byId(c.classroomids)),
+        classroom: selectedClassroom,
+      })
+    }).sort((a, b) => (a.period as any) - (b.period as any))
 }
